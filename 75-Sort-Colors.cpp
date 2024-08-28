db5ba2d81d758af0016a546a90879efbc5d43036
Rahul Sahani <112472393+rahulksahani810@users.noreply.github.com>
2024-08-28 10:44:47 +0530

    [75] [Time Beats: 100.00%] [Memory Beats: 12.99%] - LeetPush

--- 75-Sort-Colors.cpp
+++ 75-Sort-Colors.cpp
@@ -2,25 +2,25 @@ class Solution {
 public:
     void sortColors(vector<int>& nums) {
        int n = nums.size();
-       int count1=0;
-       int count2=0;
-       int count3 = 0;
-       for(int i=0;i<n;i++){
-        if(nums[i] == 0) count1++;
-        else if(nums[i] == 1) count2++;
-        else count3++;
-       }
-       cout<<count1<<\ \<<count2<<\ \<<count3;
-       for(int i=0;i<count1;i++){
-        nums[i] = 0;
-       }
-       for(int i=count1;i<count1+count2;i++){
-        nums[i] = 1;
-       }
-       for(int i=count2 +count1;i<n;i++){
-        nums[i] = 2;
-       }
+       int low =0;
+       int mid = 0;
+       int high = n-1;
+       while(mid <= high){
+        if(nums[mid] == 0){
+            swap(nums[mid],nums[low]);
+            mid++;
+            low++;
 
+        }
+        else if(nums[mid]==1){
+            mid++;
+
+        }
+        else{
+            swap(nums[high],nums[mid]);
+            high--;
+        }
+       }
         
     }
 };
