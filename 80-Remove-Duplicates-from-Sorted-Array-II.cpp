5e2bbe6187279221fa6d41b5221c93a79579557a
Rahul Sahani <112472393+rahulksahani810@users.noreply.github.com>
2024-09-04 22:25:50 +0530

    [80] [Time Beats: 26.26%] [Memory Beats: 67.54%] - LeetPush

---
+++ 80-Remove-Duplicates-from-Sorted-Array-II.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int count=1;
+        int j=1;
+        for(int i=1;i<nums.size();i++){
+            if(nums[i-1] == nums[i]){
+                count++;
+
+            }
+            else{ 
+                count = 1;
+            }
+
+            if(count <= 2){
+                nums[j++] = nums[i];
+            }
+            
+        }
+        return j;
+        
+    }
+};
